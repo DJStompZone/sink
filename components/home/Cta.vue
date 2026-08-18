c3bfe080b1ee71b9490ac0364e3d451bddc3829d
DJ Magar <85457381+DJStompZone@users.noreply.github.com>
2026-08-18 18:54:21 -0500

    Update Cta.vue for text and link changes

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -3,18 +3,18 @@
     class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
   >
     <h2 class="text-4xl tracking-tight text-white md:text-6xl">
-      Deployment immediately
+      Immediate deployment
     </h2>
     <p class="mt-4 text-lg text-slate-400 md:text-xl">
-      With just a few simple clicks, you can start deploying without any expenses.
+      With just a few clicks, you can start shortening links <strong>for free</strong>.
     </p>
     <div class="flex mt-5">
       <HomeLink
-        href="https://github.com/ccbikai/sink?tab=readme-ov-file#%EF%B8%8F-deployment"
+        href="https://github.com/DJStompZone/sink?tab=readme-ov-file#%EF%B8%8F-deployment"
         type="inverted"
         title="Start Deploy"
       >
-        Start Deploy
+        Deploy Now
       </HomeLink>
     </div>
   </div>
